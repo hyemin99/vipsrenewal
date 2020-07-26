f0cd634fd52d2c292481965142f99cf52cee30f1
김혜민 <67553458+hyemin99@users.noreply.github.com>
2020-07-26 22:59:54 +0900

    modified header

--- js/vipsrenewal.js
+++ js/vipsrenewal.js
@@ -1,3 +1,64 @@
 $(function(){
     fn_article3('nav-rolling', 'rolling-btn',true);
+
+    $('.center-nav').on('mouseenter', function(){
+        $('.gnb-menu-container:not(:animated)').slideDown(function(){
+            $('.nav-underline').css({
+                'display' : 'block'
+            });
+        });
+    }).on('mouseleave', function(){
+        $('.gnb-menu-container:not(:animated)').slideUp(function(){
+            $('.nav-underline').css({
+                'display' : 'none'
+            });
+        });
+    });
+    
+    $(".gnb-menu li").each(function() {
+        $(this).on("mouseenter", function () {
+            var navList = ['01', '02', '03', '04', '05'];
+		    var classlist = $(this).attr('class');
+		    for (var navCnt=0;navCnt<navList.length;navCnt++) {
+                if (classlist.indexOf(navList[navCnt]) != -1) {
+                    $('.gnb-type li:nth-child('+(navCnt+1)+')').addClass("on");
+                }
+            }
+        }).on("mouseleave", function () {
+            var navList = ['01', '02', '03', '04', '05'];
+		    var classlist = $(this).attr('class');
+		    for (var navCnt=0;navCnt<navList.length;navCnt++) {
+                if (classlist.indexOf(navList[navCnt]) != -1) {
+                    $('.gnb-type li:nth-child('+(navCnt+1)+')').removeClass("on");
+                }
+            }
+        });
+      });
+  
+    // $('.gnb-menu li:nth-child(1)').on('mouseenter', function(){
+    //     $('.gnb-type li:nth-child(1)').addClass('on');
+    // }).on('mouseleave', function(){
+    //     $('.gnb-type li:nth-child(1)').removeClass('on');
+    // });
+    // $('.gnb-menu li:nth-child(2)').on('mouseenter', function(){
+    //     $('.gnb-type li:nth-child(2)').addClass('on');
+    // }).on('mouseleave', function(){
+    //     $('.gnb-type li:nth-child(2)').removeClass('on');
+    // });
+    // $('.gnb-menu li:nth-child(3)').on('mouseenter', function(){
+    //     $('.gnb-type li:nth-child(3)').addClass('on');
+    // }).on('mouseleave', function(){
+    //     $('.gnb-type li:nth-child(3)').removeClass('on');
+    // });
+    // $('.gnb-menu li:nth-child(4)').on('mouseenter', function(){
+    //     $('.gnb-type li:nth-child(4)').addClass('on');
+    // }).on('mouseleave', function(){
+    //     $('.gnb-type li:nth-child(4)').removeClass('on');
+    // });
+    // $('.gnb-menu li:nth-child(5)').on('mouseenter', function(){
+    //     $('.gnb-type li:nth-child(5)').addClass('on');
+    // }).on('mouseleave', function(){
+    //     $('.gnb-type li:nth-child(5)').removeClass('on');
+    // });
+    
 });
