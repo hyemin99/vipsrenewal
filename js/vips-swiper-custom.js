6a0d56c629dc18596ed562d0c46a99655e29c750
김혜민 <67553458+hyemin99@users.noreply.github.com>
2020-08-01 06:21:59 +0900

    modified vips best menu slide

--- js/vips-swiper-custom.js
+++ js/vips-swiper-custom.js
@@ -20,14 +20,27 @@ $(function(){
 	var swiper = new Swiper('.best-menu-right', {
 		slidesPerView: 1,
 		spaceBetween: 30,
+		loop: true,
 		pagination: {
 		  el: '.best-menu-pagination',
 		  clickable: true,
-		},
+		}
 	  });
 
+	swiper.on('slideChange', function () {
+        var slideIndex = this.activeIndex;
+		var slideTextIndex = 0;
 
-
-
+        if (slideIndex == 1 || slideIndex == 6) slideTextIndex = 0;
+        else if (slideIndex == 2) slideTextIndex = 1;
+        else if (slideIndex == 3) slideTextIndex = 2;
+        else if (slideIndex == 4) slideTextIndex = 3;
+		else if (slideIndex == 5 || slideIndex == 0) slideTextIndex = 4;
+		
+        $('.best-menu-img-list li').each(function(){
+            $(this).removeClass('on');
+        });
+		$('.best-menu-img-list li:nth-child('+(slideTextIndex+1)+')').addClass('on');
+    });
 
 });
