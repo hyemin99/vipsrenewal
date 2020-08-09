5b86a0a70cab8f0bfdbdbb38d2b35fe832cd3a13
김혜민 <67553458+hyemin99@users.noreply.github.com>
2020-08-09 14:34:10 +0900

    modified join page

--- js/join.js
+++ js/join.js
@@ -1,5 +1,21 @@
 $(function(){
+    $('.modal-btn').on('click',function () {
+        $(".modal-container").css('display', 'block');
+        $('#container').on('scroll touchmove mousewheel', function(event) { 
+            event.preventDefault();     
+            event.stopPropagation();     
+            return false; 
+        });
+	});
+    $('.modal-back, .modal-close').on('click',function () {
+        $(".modal-container").css('display', 'none');
+        $('#container').off('scroll touchmove mousewheel');
+    });
+    
+
     $('.check-all').click(function(){
         $('.li-check').prop('checked', this.checked );
     });
+
+
 });
